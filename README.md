2c501031c0e24eccc5f305128d55c9cb7c4a6bfc
Ajith <15799ak@gmail.com>
2025-06-14 16:57:44 +0530

    new single file added

---
+++ README.md
@@ -0,0 +1,11 @@
+# DemoApp
+Demo Java application for testing purpose
+
+
+With this application you can register the courses you want 
+
+
+**Command for running the docker-compose:**
+
+docker-compose -f Reg-docker-compose.yaml up
+
---
+++ src/main/java/com/mvn/Registration/Services/CourseService.java
@@ -0,0 +1,35 @@
+package com.mvn.Registration.Services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mvn.Registration.Entity.Course;
+import com.mvn.Registration.Repository.CourseRepository;
+
+import jakarta.annotation.PostConstruct;
+import java.util.List;
+
+@Service
+public class CourseService {
+
+    @Autowired
+    private CourseRepository courseRepo; 
+
+    @PostConstruct
+    public void loadDefaultCourses() {
+        if (courseRepo.count() == 0) {  // Ensure data is only added once
+            courseRepo.save(new Course("Java", "3 Months"));
+            courseRepo.save(new Course("Python", "4 Months"));
+            courseRepo.save(new Course("Web Development", "6 Months"));
+            courseRepo.save(new Course("C++", "3 Months"));
+        }
+    }
+
+    public Course addCourse(Course course) {
+        return courseRepo.save(course);
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepo.findAll();
+    }
+}
